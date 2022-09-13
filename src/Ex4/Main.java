c4e4dbe72c462acccc77f0920ff97f50e8a867f6
Mihai Voican <mihai.voican@rarom.ro>
2022-09-13 14:16:07 +0300

    Commit corectii

--- src/Ex4/Main.java
+++ src/Ex4/Main.java
@@ -2,20 +2,20 @@ package Ex4;
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student("Jon", 4);
-        System.out.println(String.format("Adaugat %s, clasa %s. Nr total studenti: %s, clasa medie: %s",
+        Student s1 = new Student("Jon", 6);
+        System.out.println(String.format("Adaugat %s, nota %s. Nr total studenti: %s, nota medie: %s",
                 s1.getName(),
                 s1.getGrade(),
                 Student.noOfStudents,
                 Student.avgGradeOfStudents()));
-        Student s2 = new Student("James",12);
-        System.out.println(String.format("Adaugat %s, clasa %s. Nr total studenti: %s, clasa medie: %s",
+        Student s2 = new Student("James",10);
+        System.out.println(String.format("Adaugat %s, nota %s. Nr total studenti: %s, nota medie: %s",
                 s2.getName(),
                 s2.getGrade(),
                 Student.noOfStudents,
                 Student.avgGradeOfStudents()));
-        Student s3 = new Student("Mircea",1);
-        System.out.println(String.format("Adaugat %s, clasa %s. Nr total studenti: %s, clasa medie: %s",
+        Student s3 = new Student("Mircea",8);
+        System.out.println(String.format("Adaugat %s, nota %s. Nr total studenti: %s, nota medie: %s",
                 s3.getName(),
                 s3.getGrade(),
                 Student.noOfStudents,
